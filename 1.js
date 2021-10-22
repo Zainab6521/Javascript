f75d0ab7171b2b52b90274586da730c196b140ad
zainab raza <zk.652162@gmail.com>
2021-10-22 13:10:45 +0500

    Values and Variables

---
+++ 1.js
@@ -0,0 +1,13 @@
+let js = "amazing";
+
+if (js === "amazing"){
+    console.log("Js is AMAZING BRUH!")
+    console.log(`I'm ${js}`)
+}
+
+let country = "Pakistan";
+let continent = "ASIA";
+let rank = "4th"
+let population = "220.9 Million"
+
+console.log(`I'm from ${country} It's located in sub-continent ${continent} It's the World's ${rank} Most Populous country With the Population of almost ${population}`)
