7eb0f9945bb0ede4a0e4852bb76250da119b7dc6
zainab raza <zk.652162@gmail.com>
2021-10-22 15:21:14 +0500

    7 DATA TYPES in JAVASCRIPT

--- 1.js
+++ 1.js
@@ -8,6 +8,12 @@ if (js === "amazing"){
 let country = "Pakistan";
 let continent = "ASIA";
 let rank = "4th"
-let population = "220.9 Million"
+let population = 229.0 ;
 
-console.log(`I'm from ${country} It's located in sub-continent ${continent} It's the World's ${rank} Most Populous country With the Population of almost ${population}`)
+console.log(`I'm from ${country} It's located in sub-continent ${continent} It's the World's ${rank} Most Populous country With the Population of almost ${population} millions population`)
+
+
+//Data Types
+let isIsland = false;
+let language;
+console.log(typeof country, typeof isIsland, typeof language, typeof population);
