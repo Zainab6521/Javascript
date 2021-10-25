9c155f148a5f3a5be2e143e6c3f366f5dbaaf64f
zainab raza <zk.652162@gmail.com>
2021-10-25 11:36:25 +0500

    Functions

--- fundamentals-2.js
+++ fundamentals-2.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function fruitprocessor(oranges, grapes){
+    console.log("oranges",oranges,"grapes",grapes);
+    const juice = `I'm making juice of ${ oranges} oranges and ${grapes} grapes.`
+    return juice;
+}
+
+const freshJuice = fruitprocessor(5,4);
+const orangesJuice = fruitprocessor(7,0);
+console.log(freshJuice);
+console.log(orangesJuice)
+
+
+function describeCountry(country, population, capitalCity){
+    return`${country} has ${population} people and its capital city is ${capitalCity}. `
+    ;
+}
+const descPakistan = describeCountry("pakistan","229.0 million","Islamabad")
+const descChina = describeCountry("China", "1.402 billion", "beijing")
+const descSingapore = describeCountry("Singapore","32.37 million","Kuala Lumpur")
+console.log(descPakistan,descChina,descSingapore)
