e888d98a0476ed45824519de39899ae225cb58b8
zainab raza <zk.652162@gmail.com>
2021-11-02 12:38:19 +0500

    calculated Amplitude with multiple temp arrays.

--- script.js
+++ script.js
@@ -13,8 +13,30 @@
 // printForecast(arr);
 
 //problem for calculate Amplitude
-const temperatures = [-3, -7, -9, -1, 'error', 4, 8, 5, 19];
-const calcAmplitude = temps => {
+// const temperatures = [-3, -7, -9, -1, 'error', 4, 8, 5, 19];
+// const calcAmplitude = temps => {
+//   let max = temps[0];
+//   let min = temps[0];
+
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== 'number') continue;
+
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   // console.log(max, min);
+//   return max - min;
+// };
+// const amplitude = calcAmplitude(temperatures);
+// console.log(amplitude);
+
+//same problem as above with 2 Arrays
+// const temperatures = [-3, -7, -9, -1, 'error', 4, 8, 5, 19];
+const t1 = [4, 5, 6];
+const t2 = [7, 8, 9];
+const calcAmplitude = (t1, t2) => {
+  const temps = t1.concat(t2);
   let max = temps[0];
   let min = temps[0];
 
@@ -28,5 +50,5 @@ const calcAmplitude = temps => {
   // console.log(max, min);
   return max - min;
 };
-const amplitude = calcAmplitude(temperatures);
+const amplitude = calcAmplitude(t1, t2);
 console.log(amplitude);
