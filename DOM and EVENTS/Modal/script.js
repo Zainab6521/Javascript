b6b472386f2b85058746601ee1f89ef30a5301dd
zainab raza <zk.652162@gmail.com>
2021-11-08 12:03:56 +0500

    added functionality t o buttons.

--- DOM and EVENTS/Modal/script.js
+++ DOM and EVENTS/Modal/script.js
@@ -6,20 +6,23 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 
 const openModal = () => {
-  modal.classList.remove('.hidden');
-  overlay.classList.remove('.hidden');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 };
 const closeModal = () => {
-  modal.classList.add('.hidden');
-  overlay.classList.add('.hidden');
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 };
 for (let i = 0; i < btnOpenModal.length; i++) {
-  btnOpenModal[i].addEventListener('click', () => {
-    console.log('Button Clicked!');
-    modal.classList.remove('.hidden');
-    overlay.classList.remove('.hidden');
-  });
+  btnOpenModal[i].addEventListener('click', openModal);
 }
 
 btnCloseModal.addEventListener('click', closeModal);
-btnOpenModal.addEventListener('click', openModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', e => {
+  console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
