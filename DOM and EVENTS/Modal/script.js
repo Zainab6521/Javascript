54ca6f64923a73646efa393d9222f554e19cbfbe
zainab raza <zk.652162@gmail.com>
2021-11-08 11:36:49 +0500

    added functionality t o buttons.

--- DOM and EVENTS/Modal/script.js
+++ DOM and EVENTS/Modal/script.js
@@ -3,4 +3,21 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnOpenModal = document.querySelector('.show-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = () => {
+  modal.classList.remove('.hidden');
+  overlay.classList.remove('.hidden');
+};
+const closeModal = () => {
+  modal.classList.add('.hidden');
+  overlay.classList.add('.hidden');
+};
+for (let i = 0; i < btnOpenModal.length; i++) {
+  console.log('hi');
+  btnOpenModal[i].addEventListener('click', () => {
+    console.log('Button Clicked!');
+    modal.classList.remove('.hidden');
+    overlay.classList.remove('.hidden');
+  });
+}
