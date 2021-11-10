bd02bb185c9bb3da6fd740eab73f6f9215b76860
zainab raza <zk.652162@gmail.com>
2021-11-10 16:03:17 +0500

    Rolling dice functionality

--- DOM and EVENTS/Pick-game/script.js
+++ DOM and EVENTS/Pick-game/script.js
@@ -3,6 +3,9 @@
 //selecting Elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.querySelector('.current--0');
+const current1El = document.querySelector('.current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('btn--roll');
@@ -12,7 +15,7 @@ const btnHold = document.querySelector('btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
-
+let currentScore = 0;
 //Rolling dice functionality
 btnRoll.addEventListener('click', () => {
   //Generating a random dice roll
@@ -20,5 +23,13 @@ btnRoll.addEventListener('click', () => {
   //Display Dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice.png}`;
+
+  //check for rolled 1
+  if (dice !== 1) {
+    //Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; //change later
+  } else {
+  }
   //check for role 1: If true, Switch to player 2.
 });
