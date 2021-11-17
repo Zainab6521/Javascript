0a066b4c8ff9b3d7648b2ce77ffd2ef0266afded
zainab raza <zk.652162@gmail.com>
2021-11-17 16:51:24 +0500

    functionality of Hold Button.

--- DOM and EVENTS/Pick-game/script.js
+++ DOM and EVENTS/Pick-game/script.js
@@ -6,31 +6,39 @@ const player1El = document.querySelector('.player--1');
 
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
-const current0El = document.querySelector('.current--0');
-const current1El = document.querySelector('.current--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector('btn--roll');
-const btnHold = document.querySelector('btn--hold');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 //starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-const score = [0, 0];
-let currentScore = 0;
+const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+
+const switchPlayer = () => {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player-active');
+  player1El.classList.toggle('player-active');
+};
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', () => {
   //Generating a random dice roll
   const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
   //Display Dice
   diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice.png}`;
+  diceEl.src = `dice-${dice}.png`;
 
   //check for rolled 1
   if (dice !== 1) {
@@ -41,9 +49,19 @@ btnRoll.addEventListener('click', () => {
     current0El.textContent = currentScore; //change later
   } else {
     //Switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player-active');
-    player1El.classList.toggle('player-active');
+    switchPlayer();
   }
+});
+
+btnHold.addEventListener('click', () => {
+  //ADD current Score to active player score.
+  scores[activePlayer] += currentScore;
+  // scores[1] = scores[1] + currentScore;
+  console.log(scores[activePlayer]);
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+  //Check if player score is >=100
+  //Finish the Game
+  //Switch to the next player
+  switchPlayer();
 });
